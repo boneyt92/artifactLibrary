04324b943a2c11a62a06ea8f5eb980a914e1f47f
Bony Thomas <bony.thomas@jci.com>
2019-08-09 17:45:01 +0530

    Initial Commit

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArtifactLibrary
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        public string function1()
+        {
+            return "Function 1";
+        }
+
+        public string function2()
+        {
+            return "Function 2";
+        }
+    }
+}
